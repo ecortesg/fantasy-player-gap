dc00f0291e07484621ce4fc7ae7dcaf316e39f4d
ecortesg <cgarcia.ernesto@gmail.com>
2024-02-13 13:16:26 -0600

    Merged components

--- src/components/DraftBoard.jsx
+++ src/components/DraftBoard.jsx
@@ -1,7 +1,6 @@
 import { useDraftSettingsStore } from "../store/draftSettingsStore";
 import { useDraftStore } from "../store/draftStore";
 import { arrayRange } from "../utils";
-import BoardCard from "./BoardCard";
 import { useEffect } from "react";
 
 function DraftBoard() {
@@ -84,3 +83,31 @@ function DraftBoard() {
 }
 
 export default DraftBoard;
+
+function BoardCard({ pick }) {
+  const colors = {
+    QB: "bg-qb",
+    RB: "bg-rb",
+    WR: "bg-wr",
+    TE: "bg-te",
+    K: "bg-k",
+    DEF: "bg-def",
+  };
+
+  return (
+    <div
+      className={`px-2 w-36 h-16 text-sm ${
+        colors[pick.player.position] || "bg-slate-300"
+      } ${
+        pick.player.isProjection ? "bg-opacity-40 opacity-50" : "bg-opacity-50"
+      } rounded-lg relative text-sm`}
+    >
+      <p className="absolute top-0 right-2">
+        {pick.round}.{pick.number}
+      </p>
+      <p>{pick.player.first_name}</p>
+      <p>{pick.player.last_name}</p>
+      <p>{pick.player.position}</p>
+    </div>
+  );
+}
